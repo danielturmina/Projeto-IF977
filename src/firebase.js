ec110c7e8c71b0885efced76549f2c2fddfc104e
Giovaniavs <gavs2@cin.ufpe.br>
2021-08-04 19:59:19 -0300

    change the base project

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase'
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCxXpuRv3hOcEH6bVqwBS3d9LixnBSEvGE",
+  authDomain: "if977-plataformaweb.firebaseapp.com",
+  projectId: "if977-plataformaweb",
+  storageBucket: "if977-plataformaweb.appspot.com",
+  messagingSenderId: "657063684926",
+  appId: "1:657063684926:web:4f4db33407527d54695a6f"
+};
+
+  // Initialize Firebase
+const fire = firebase.initializeApp(firebaseConfig);
+
+export default fire;    
