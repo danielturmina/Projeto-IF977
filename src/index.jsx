b904ef7a80e6b8ce97a891938ac3d635dc303423
Yuri Barros <yuricdb@gmail.com>
2021-08-09 18:56:26 -0300

    Mudança
    Primeira

--- src/index.jsx
+++ src/index.jsx
@@ -5,6 +5,7 @@ import GlobalStyle from './global/globalStyle';
 
 import IsLogged from './Views/IsLogged/IsLogged';
 import UserProfile from './Views/UserProfile/UserProfile';
+import EndPag from './Views/EndPag/endPag';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Switch>
             <Route component={IsLogged} exact path="/" />
             <Route component={UserProfile} exact path="/profile" />
+            <Route component={EndPag} exact path="/endpag" />
           </Switch>
         </Router>
         <GlobalStyle />
